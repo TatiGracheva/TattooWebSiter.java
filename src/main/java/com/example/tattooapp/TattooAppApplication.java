36257b3a9440bfe826ea2bd97353fd63717428ed
TatiGracheva <tatjan_zakharova@mail.ru>
2023-08-26 12:20:09 +0300

    Initial commit

---
+++ src/main/java/com/example/tattooapp/TattooAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.tattooapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TattooAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TattooAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tattooapp/TattooAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tattooapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TattooAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
